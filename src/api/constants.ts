851664fea9bc9848688e18914ff338f4b89f4e95
SaraAlsukary <sarals6216@gmail.com>
2026-04-09 08:25:56 +0300

    update constants

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,3 @@
-const APIURL: string = "http://localhost:8000/api";
-// const APIURL: string = "https://api.japaneseacademy.jp/api";
+// const APIURL: string = "http://localhost:8000/api";
+const APIURL: string = "https://api.japaneseacademy.jp/api";
 export default APIURL;
